9677eb3a0595ec76091ab5039e51169f5454763c
fon-varvara <136700192+fon-varvara@users.noreply.github.com>
2023-06-20 17:31:58 +0700

    Refactor code for scalability

---
+++ elephant_queen_waterfall.scala
@@ -0,0 +1,63 @@
+object ThoughtForFood {
+  def main(args: Array[String]): Unit = {
+    // 1. Appreciate the food you have
+    println("Be grateful for the food you have today.")
+
+    // 2. Keep track of your eating habits
+    println("Take the time to keep a log of what you eat.")
+
+    // 3. Eat whole foods
+    println("Choose whole foods over processed foods whenever possible.")
+
+    // 4. Prepare healthy meals
+    println("Make an effort to prepare meals that are good for your health.")
+
+    // 5. Focus on your eating
+    println("Take the time to eat slowly and focus on your food.")
+
+    // 6. Eat enough fruits and vegetables
+    println("Try to have at least 5 fruits and vegetables everyday.")
+
+    // 7. Eat a wide variety of foods
+    println("Don't limit yourself to a few types of foods, try something new every now and then.")
+
+    // 8. Avoid sugary drinks
+    println("Minimize your intake of sugary drinks such as soda or energy drinks.")
+
+    // 9. Limit processed and junk foods
+    println("Be mindful of the amount of processed and junk foods you eat.")
+
+    // 10. Listen to your body
+    println("Pay attention to when your body is full - don't overeat.")
+
+    // 11. Eat slowly
+    println("Take your time while eating. Put down your fork between bites.")
+
+    // 12. Avoid eating out of boredom
+    println("Be aware of when you're eating out of boredom and try to distract yourself with something else.")
+
+    // 13. Avoid eating distracted
+    println("Don't eat while watching TV or playing video games - this can lead to overeating.")
+
+    // 14. Choose healthy snacks
+    println("Replace unhealthy snacks such as candy and chips with healthier options such as fruits and nuts.")
+
+    // 15. Don't eat until you feel stuffed
+    println("Stop eating before you're full - overeating can lead to weight gain.")
+
+    // 16. Don't judge yourself
+    println("Don't judge yourself if you make a mistake - learn from it and move on.")
+
+    // 17. Don't skip meals
+    println("Skipping meals can lead to overeating later on - try to have 3 meals a day.")
+
+    // 18. Drink enough water
+    println("Drink enough water to stay hydrated - aim for 8 glasses a day.")
+
+    // 19. Get enough sleep
+    println("Make sure you get enough sleep - having a regular sleep schedule is important for your health.")
+
+    // 20. Avoid eating too late
+    println("Try to finish eating before 8pm - late night snacking can interfere with your sleep.")
+  }
+}
