829f2174cd2c791411389094e895a9b556750867
fon-varvara <136700192+fon-varvara@users.noreply.github.com>
2023-06-25 15:46:40 +0700

    Fix memory management issues

---
+++ island_banana_apple.js
@@ -0,0 +1,513 @@
+const foodForThought = () => {
+    // 1
+    let food = "Cereal";
+
+    // 2
+    let thought = "Everyone needs breakfast to start their day right.";
+
+    // 3
+    console.log(`${food}: ${thought}`);
+
+    // 4
+    food = "Sandwich";
+
+    // 5
+    thought = "Eating lunch can make a person feel energized after a morning of work.";
+
+    // 6
+    console.log(`${food}: ${thought}`);
+
+    // 7
+    food = "Cake";
+
+    // 8
+    thought = "Enjoy a sweet treat after dinner to finish off your meal.";
+
+    // 9
+    console.log(`${food}: ${thought}`);
+
+    // 10
+    food = "Salad";
+
+    // 11
+    thought = "Including a salad in your meal can load your body with the vitamins it needs to stay healthy.";
+
+    // 12
+    console.log(`${food}: ${thought}`);
+
+    // 13
+    food = "Pizza";
+
+    // 14
+    thought = "Turn a family night into something special with an amazing pizza!";
+
+    // 15
+    console.log(`${food}: ${thought}`);
+
+    // 16
+    food = "Soup";
+
+    // 17
+    thought = "Soup can be a light meal or the start to a more hearty dinner.";
+
+    // 18
+    console.log(`${food}: ${thought}`);
+
+    // 19
+    food = "Stir Fry";
+
+    // 20
+    thought = "Stir fry can be a quick weeknight meal that is packed with nutrition.";
+
+    // 21
+    console.log(`${food}: ${thought}`);
+
+    // 22
+    food = "Pasta";
+
+    // 23
+    thought = "Pasta can make a great base for any dish, just adding your favorite ingredients can make it unique.";
+
+    // 24
+    console.log(`${food}: ${thought}`);
+
+    // 25
+    food = "Cheese";
+
+    // 26
+    thought = "Cheese can make anything taste better, the possibilities are endless when it comes to cooking with cheese.";
+
+    // 27
+    console.log(`${food}: ${thought}`);
+
+    // 28
+    food = "Bread";
+
+    // 29
+    thought = "Freshly baked bread can be the perfect accompaniment to any meal.";
+
+    // 30
+    console.log(`${food}: ${thought}`);
+
+    // 31
+    food = "Fruit";
+
+    // 32
+    thought = "Fruit can be a great snack anytime of day - it's also a great way to get your daily dose of vitamins.";
+
+    // 33
+    console.log(`${food}: ${thought}`);
+
+    // 34
+    food = "Ice Cream";
+
+    // 35
+    thought = "Ice cream is great for a treat on a hot day, and it can also be used to top off your favorite dessert.";
+
+    // 36
+    console.log(`${food}: ${thought}`);
+
+    // 37
+    food = "Chips";
+
+    // 38
+    thought = "Chips can make a great appetizer or snack, why not try a new flavor to mix up your routine.";
+
+    // 39
+    console.log(`${food}: ${thought}`);
+
+    // 40
+    food = "Sushi";
+
+    // 41
+    thought = "Sushi can be a great way to enjoy a healthy meal when you're looking for something light!";
+
+    // 42
+    console.log(`${food}: ${thought}`);
+
+    // 43
+    food = "Noodles";
+
+    // 44
+    thought = "Noodles can be a quick and easy meal when you're in a rush!";
+
+    // 45
+    console.log(`${food}: ${thought}`);
+
+    // 46
+    food = "Curry";
+
+    // 47
+    thought = "Curry is a great way to add some flavor to any meal!";
+
+    // 48
+    console.log(`${food}: ${thought}`);
+
+    // 49
+    food = "Hamburger";
+
+    // 50
+    thought = "A classic hamburger can make a great lunch on it's own or can be part of a more hearty meal.";
+
+    // 51
+    console.log(`${food}: ${thought}`);
+
+    // 52
+    food = "Cookies";
+
+    // 53
+    thought = "Cookies can make a great dessert, or a sweet snack in between meals.";
+
+    // 54
+    console.log(`${food}: ${thought}`);
+
+    // 55
+    food = "Eggs";
+
+    // 56
+    thought = "Eggs can make a great breakfast in the morning, or can be a light dinner in the evening.";
+
+    // 57
+    console.log(`${food}: ${thought}`);
+
+    // 58
+    food = "Yogurt";
+
+    // 59
+    thought = "Adding some yogurt to your meal can add some protein and calcium to the mix!";
+
+    // 60
+    console.log(`${food}: ${thought}`);
+
+    // 61
+    food = "Tofu";
+
+    // 62
+    thought = "Tofu can make a great substitution in many recipes - making them healthier for your body.";
+
+    // 63
+    console.log(`${food}: ${thought}`);
+
+    // 64
+    food = "Smoothie";
+
+    // 65
+    thought = "Smoothies can make a great snack or breakfast - just add your favorite ingredients and blend away!";
+
+    // 66
+    console.log(`${food}: ${thought}`);
+
+    // 67
+    food = "Fish";
+
+    // 68
+    thought = "Fish can make a healthy and delicious meal - just add some vegetables and you have a meal!";
+
+    // 69
+    console.log(`${food}: ${thought}`);
+
+    // 70
+    food = "Mexican";
+
+    // 71
+    thought = "Mexican food is a great way to spice up your meals - enjoy as a single meal or pair with a side dish.";
+
+    // 72
+    console.log(`${food}: ${thought}`);
+
+    // 73
+    food = "Chinese";
+
+    // 74
+    thought = "Chinese food can make a great dinner for the family - don't be afraid to try something new!";
+
+    // 75
+    console.log(`${food}: ${thought}`);
+
+    // 76
+    food = "Indian";
+
+    // 77
+    thought = "Indian food can be a great way to get some spice in your life - try some curries to really liven up your meals!";
+
+    // 78
+    console.log(`${food}: ${thought}`);
+
+    // 79
+    food = "Italian";
+
+    // 80
+    thought = "Italian food can make a great dinner for the family - don't forget the garlic bread!";
+
+    // 81
+    console.log(`${food}: ${thought}`);
+
+    // 82
+    food = "Pastries";
+
+    // 83
+    thought = "Enjoy a sweet pastry with your morning coffee - you don't need an excuse to treat yourself!";
+
+    // 84
+    console.log(`${food}: ${thought}`);
+
+    // 85
+    food = "Rice";
+
+    // 86
+    thought = "Rice can make a great side dish to your meals - it's also a great way to use up any leftovers you may have!";
+
+    // 87
+    console.log(`${food}: ${thought}`);
+
+    // 88
+    food = "Popcorn";
+
+    // 89
+    thought = "Popcorn is a great snack for a night in - you can also try adding different flavors to mix it up!";
+
+    // 90
+    console.log(`${food}: ${thought}`);
+
+    // 91
+    food = "Grill";
+
+    // 92
+    thought = "Fire up the grill and make some delicious burgers for dinner - you can even mix in some vegetables to make them healthier!";
+
+    // 93
+    console.log(`${food}: ${thought}`);
+
+    // 94
+    food = "Fried Chicken";
+
+    // 95
+    thought = "Fried chicken can make a great addition to any meal - it's also a great way to get your protein!";
+
+    // 96
+    console.log(`${food}: ${thought}`);
+
+    // 97
+    food = "Wraps";
+
+    // 98
+    thought = "Wraps can make a great lunch - just fill them up with whatever you're craving!";
+
+    // 99
+    console.log(`${food}: ${thought}`);
+
+    // 100
+    food = "Barbecue";
+
+    // 101
+    thought = "Fire up the barbecue for a fun night grilling out with friends and family - just don't forget the side dishes!";
+
+    // 102
+    console.log(`${food}: ${thought}`);
+
+    // 103
+    food = "Smoked Salmon";
+
+    // 104
+    thought = "Smoked salmon can be a great addition to any meal - try adding it to your salad for an extra boost of protein!";
+
+    // 105
+    console.log(`${food}: ${thought}`);
+
+    // 106
+    food = "Stuffed Peppers";
+
+    // 107
+    thought = "Stuffed peppers are a great way to enjoy vegetables - fill them up with whatever ingredients you'd like!";
+
+    // 108
+    console.log(`${food}: ${thought}`);
+
+    // 109
+    food = "Burrito";
+
+    // 110
+    thought = "Enjoy a burrito for lunch - just don't forget the chips and guacamole!";
+
+    // 111
+    console.log(`${food}: ${thought}`);
+
+    // 112
+    food = "Taco";
+
+    // 113
+    thought = "Tacos can make a fun and easy dinner for the family - just add your favorite toppings!";
+
+    // 114
+    console.log(`${food}: ${thought}`);
+
+    // 115
+    food = "Donut";
+
+    // 116
+    thought = "Enjoy a donut for a sweet treat - they can make a great breakfast or snack!";
+
+    // 117
+    console.log(`${food}: ${thought}`);
+
+    // 118
+    food = "Fries";
+
+    // 119
+    thought = "Fries can make a great accompaniment to any meal - just don't forget to dip!";
+
+    // 120
+    console.log(`${food}: ${thought}`);
+
+    // 121
+    food = "Quesadilla";
+
+    // 122
+    thought = "Quesadillas are a great way to use up leftovers - just mix them up with some cheese and enjoy!";
+
+    // 123
+    console.log(`${food}: ${thought}`);
+
+    // 124
+    food = "Tart";
+
+    // 125
+    thought = "Tarts can make a great dessert - just add your favorite fruit and enjoy!";
+
+    // 126
+    console.log(`${food}: ${thought}`);
+
+    // 127
+    food = "Mac and Cheese";
+
+    // 128
+    thought = "Mac and cheese can make a great comfort food - it's also a great way to use up any leftover cheese you have!";
+
+    // 129
+    console.log(`${food}: ${thought}`);
+
+    // 130
+    food = "Pie";
+
+    // 131
+    thought = "Pies can make a great dessert, or a light snack - just make sure to get the perfect crust!";
+
+    // 132
+    console.log(`${food}: ${thought}`);
+
+    // 133
+    food = "Muffin";
+
+    // 134
+    thought = "Muffins can make a great breakfast or snack - just make sure to get the perfect texture!";
+
+    // 135
+    console.log(`${food}: ${thought}`);
+
+    // 136
+    food = "Oatmeal";
+
+    // 137
+    thought = "Oatmeal can make a great breakfast, or a light snack - just add some fresh fruit and enjoy!";
+
+    // 138
+    console.log(`${food}: ${thought}`);
+
+    // 139
+    food = "Chocolate";
+
+    // 140
+    thought = "Chocolate can make a great treat - it's also a great way to get your daily antioxidants!";
+
+    // 141
+    console.log(`${food}: ${thought}`);
+
+    // 142
+    food = "Coffee";
+
+    // 143
+    thought = "Coffee can make a great pick-me-up in the morning - just make sure to get your flavor just right!";
+
+    // 144
+    console.log(`${food}: ${thought}`);
+
+    // 145
+    food = "Crepes";
+
+    // 146
+    thought = "Crepes are a great breakfast - just add your favorite toppings and enjoy!";
+
+    // 147
+    console.log(`${food}: ${thought}`);
+
+    // 148
+    food = "Pop Tart";
+
+    // 149
+    thought = "Pop tarts can make a great snack - just heat them up and enjoy!";
+
+    // 150
+    console.log(`${food}: ${thought}`);
+
+    // 151
+    food = "Crab";
+
+    // 152
+    thought = "Crab can make a great addition to any meal - just make sure to get the freshest crab you can find!";
+
+    // 153
+    console.log(`${food}: ${thought}`);
+
+    // 154
+    food = "Shrimp";
+
+    // 155
+    thought = "Shrimp can make a great addition to any meal - just add your favorite spices and enjoy!";
+
+    // 156
+    console.log(`${food}: ${thought}`);
+
+    // 157
+    food = "Fruit Salad";
+
+    // 158
+    thought = "Fruit salad can make a great snack - just mix up your favorite fruits and enjoy!";
+
+    // 159
+    console.log(`${food}: ${thought}`);
+
+    // 160
+    food = "Clam Chowder";
+
+    // 161
+    thought = "Clam chowder can make a great dinner - just serve with some crusty bread and enjoy!";
+
+    // 162
+    console.log(`${food}: ${thought}`);
+
+    // 163
+    food = "Granola Bar";
+
+    // 164
+    thought = "Granola bars can make a great snack - just grab one to get you through the day!";
+
+    // 165
+    console.log(`${food}: ${thought}`);
+
+    // 166
+    food = "Pancakes";
+
+    // 167
+    thought = "Pancakes can make a great breakfast - just top them with your favorite syrup and enjoy!";
+
+    // 168
+    console.log(`${food}: ${thought}`);
+
+    // 169
+    food = "Mashed Potatoes";
+
+    // 170
+    thought = "Mashed potatoes can make a great side dish - just add some butter and enjoy!";
+
+    // 171
+    console.log(`${food}: ${thought}
